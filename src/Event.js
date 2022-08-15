d46c74cf391183f7a1456b57cc563d01db094a97
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-08-15 06:56:12 +0200

    extended event component

--- src/Event.js
+++ src/Event.js
@@ -2,7 +2,25 @@ import React, { Component } from 'react';
 
 class Event extends Component {
   render() {
-    return <div></div>
+    const { summary } = this.props;
+    const { description } = this.props;
+    const { startDateTime } = this.props.start.dateTime;
+    const { startTimeZone } = this.props.start.timeZone;
+    const { location } = this.props.location;
+    const { detailsGoogleCalendar } = this.props.htmlLink;
+
+
+    return (
+      <div>
+        <div>{description}</div>
+        <div>{startDateTime}</div>
+        <div>{startTimeZone}</div>
+        <div>{location}</div>
+        <div>{detailsGoogleCalendar}</div>
+        <div>{summary}</div>
+
+      </div>
+    )
   }
 }
 
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Event from '../Event';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import { mockData } from '../mockData';

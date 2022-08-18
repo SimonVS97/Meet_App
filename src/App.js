c7c36ac05d1fbadd1146e8b51bbeba1e24f8d997
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-08-18 07:13:41 +0200

    added test for suggestions

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySeach';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents } from './api';
+import { extractLocations, getEvents } from './api';
 
 
 class App extends Component {
@@ -15,11 +15,26 @@ class App extends Component {
 
   updateEvents = (location) => {
     getEvents().then((events) => {
-      const locationEvents = events.filter((event) => event.location === location);
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
       this.setState({
         events: locationEvents
       });
     });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
   }
 
   render() {
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -59,5 +59,14 @@ describe('<App /> integration', () => {
     const eventsToShow = allEvents.filter(event => event.location === selectedCity);
     expect(AppWrapper.state('events')).toEqual(eventsToShow);
     AppWrapper.unmount();
+  });
+  test('get list of events when user selects "See all cities"', async () => {
+    const AppWrapper = mount(<App />);
+    const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
+    await suggestionItems.at(suggestionItems.length - 1).simulate('click');
+    const allEvents = await getEvents();
+    expect(AppWrapper.state('events')).toEqual(allEvents);
+    AppWrapper.unmount();
+
   })
 })

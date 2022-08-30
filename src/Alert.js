18fa756875a6c70c04f7e3eae881d9fadaead69d
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-08-30 07:31:01 -0700

    added Offline alert

--- src/Alert.js
+++ src/Alert.js
@@ -35,5 +35,13 @@ class ErrorAlert extends Alert {
   }
 }
 
+class OfflineAlert extends Alert {
+  constructor(props) {
+    super(props);
+    this.color = 'yellow';
+  }
+}
+
 export { InfoAlert };
 export { ErrorAlert };
+export { OfflineAlert };
--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Component } from 'react';
-import Event from './Event.js'
+import Event from './Event.js';
+import { OfflineAlert } from './Alert.js';
 
 class EventList extends Component {
   render() {
     const { events } = this.props;
     return (
       <ul className='EventList'>
+        <li>
+          {!navigator.onLine && OfflineAlert}
+        </li>
         {events.map(event =>
           <li key={event.id}>
             <Event event={event} />

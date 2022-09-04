0db260bbee73a66ef05aa3c6eab693b3403fde81
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-09-04 16:36:56 -0700

    fix pue chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -2,18 +2,28 @@ import React, { useEffect, useState } from 'react';
 import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
-  const data = [
-    { name: 'Group A', value: 400 },
-    { name: 'Group B', value: 300 },
-    { name: 'Group C', value: 300 },
-    { name: 'Group D', value: 200 }
-  ];
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    setData(() => getData());
+  }, [events]);
+
+
+  const getData = () => {
+    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+    const data = genres.map((genre) => {
+      const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
+      return { name: genre, value };
+    });
+    return data;
+  }
 
   return (
     <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
         <Pie
-          data={data}
+          data={getData()}
           cx={200}
           cy={200}
           labelLine={false}

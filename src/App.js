cafe5c32613b201749449cccbf85644ba659c6a1
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-09-21 07:07:22 -0700

    deleted logs

--- src/App.js
+++ src/App.js
@@ -75,8 +75,6 @@ class App extends Component {
 
   getData = () => {
     const { locations, events } = this.state;
-    console.log(typeof locations);
-    console.log(locations);
     const data = locations.map((location) => {
       const number = events.filter((event) => event.location === location).length;
       const city = location.split(', ').shift();

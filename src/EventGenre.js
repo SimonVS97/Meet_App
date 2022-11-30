825a95cc4c0ce3a67c7497b1676d13010cfd92ab
SimonVS97 <104713327+SimonVS97@users.noreply.github.com>
2022-11-29 18:30:57 -0800

    fixed pie chart

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,49 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer, Legend } from 'recharts';
+import { PieChart, Pie, Cell, ResponsiveContainer, Tooltip } from 'recharts';
 
 const EventGenre = ({ events }) => {
-
   const [data, setData] = useState([]);
-  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#ba00fe'];
 
   useEffect(() => {
-    setData(() => getData());
+    const getData = () => {
+      const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
+      const data = genres.map((genre) => {
+        const value = events.filter((event) =>
+          event.summary.split(' ').includes(genre)
+        ).length;
+        return { name: genre, value };
+      });
+      return data;
+    };
+
+    setData(getData());
   }, [events]);
 
-
-  const getData = () => {
-    const genres = ['React', 'JavaScript', 'Node', 'jQuery', 'AngularJS'];
-    const data = genres.map((genre) => {
-      const value = events.filter((event) => event.summary.split(' ').includes(genre)).length;
-      return { name: genre, value };
-    });
-    return data;
-  }
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', '#ba00fe'];
 
   return (
     <ResponsiveContainer height={400}>
       <PieChart width={400} height={400}>
+        <Tooltip cursor={{ strokeDasharray: '3 3' }} />
         <Pie
-          data={getData()}
-          cx={200}
-          cy={200}
+          data={data}
+          cx="50%"
+          cy="50%"
           labelLine={false}
           outerRadius={80}
           fill="#8884d8"
           dataKey="value"
-          label={({ name, percent }) => `${name} ${(percent * 100).toFixed(0)}%`}
-        >
-          {
-            data.map((entry, index) => (
-              <Cell key={`cell-${index}`} fill={colors[index]} />
-            ))
+          label={({ name, percent }) =>
+            `${name} ${(percent * 100).toFixed(0)}%`
           }
+        >
+          {data.map((entry, index) => (
+            <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+          ))}
         </Pie>
-        <Legend verticalAlign="top" height={36} />
       </PieChart>
     </ResponsiveContainer>
-  )
-
-}
+  );
+};
 
 export default EventGenre;
